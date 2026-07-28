c7e3546d70a02a2dbe1e24b590b9ca9a6780eed9
Deyplay01 <timi4god2021@gmail.com>
2026-07-28 19:31:44 +0100

    imported the components into home

--- frontend/elzino-app/src/pages/Home.jsx
+++ frontend/elzino-app/src/pages/Home.jsx
@@ -1,9 +1,26 @@
 import "./Home.css";
+import Navbar from "../components/Navbar.jsx";
+import Hero from "../components/Hero.jsx";
+import About from "../components/About.jsx";
+import Services from "../components/Services.jsx";
+import Metrics from "../components/Metrics.jsx";
+import Projects from "../components/Projects.jsx";
+import Clients from "../components/Clients.jsx";
+import Contact from "../components/Contact.jsx";
+import Footer from "../components/Footer.jsx";
 
 function HomePage(){
     return(
         <>
-        This is the Home Page. Your html code for the Home page component goes here.
+            <Navbar />
+            <Hero />
+            <Services />
+            <About />
+            <Metrics />
+            <Projects />
+            <Clients />
+            <Contact />
+            <Footer />
         </>
     )
 }
