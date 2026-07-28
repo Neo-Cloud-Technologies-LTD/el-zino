2cf17ac44d9ab6e4d5f314e669b654f84d058da0
Deyplay01 <timi4god2021@gmail.com>
2026-07-28 18:27:17 +0100

    Added assets and new components

---
+++ frontend/elzino-app/src/components/clients.jsx
@@ -0,0 +1,11 @@
+import "./clients.css";
+
+function Client(){
+    return(
+        <>
+        This is the Client component. Your html code for the Client component goes here.
+        </>
+    )
+}
+
+export default Client;
---
+++ frontend/elzino-app/src/components/contact.jsx
@@ -0,0 +1,11 @@
+import "./contact.css";
+
+function Contact(){
+    return(
+        <>
+        This is the Contact component. Your html code for the Contact component goes here.
+        </>
+    )
+}
+
+export default Contact;
