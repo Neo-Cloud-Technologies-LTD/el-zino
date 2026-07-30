ed5a01fd1cf07e79332379eb614af570add7345b
Ibrahim .P Manasseh <ibrahim.manasseh@aun.edu.ng>
2026-07-30 12:30:23 +0100

    Refactor Navbar component with links and structure

--- frontend/elzino-app/src/components/navbar.jsx
+++ frontend/elzino-app/src/components/navbar.jsx
@@ -1,10 +1,24 @@
 import './navbar.css';
 
 function Navbar() {
+    const links = [
+    { path: "/", label: "Home" },
+    { path: "/about", label: "About" },
+    { path: "/contact", label: "Contact" },
+  ];
     return (
-        <nav>
-            Your html code for the navbar component goes here.
-        </nav>
+         <div>
+      <header>
+ <a href="El-Zino/src/App.jsx" className='logo'> <img src="/Elzino logo copy.png" alt="El-Zino Logo"className="logo-box"/></a>
+  <nav>
+  <a href="/" className="active">Home</a>
+  <a href="/services">Services</a>
+  <a href="/about">About</a>
+  <a href="/projects">Projects</a>
+  <a href="/contact">Contact</a>
+</nav>
+</header>
+    </div>
     );
 }
 
