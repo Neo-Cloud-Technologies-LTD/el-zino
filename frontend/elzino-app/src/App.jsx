d20e30cf99708b8bcf6ed1801901047848044132
daniel-web89 <danielogah89@gmail.com>
2026-08-03 12:47:38 +0100

    created a contact page

--- frontend/elzino-app/src/App.jsx
+++ frontend/elzino-app/src/App.jsx
@@ -1,15 +1,12 @@
-import './App.css'
-import HomePage from "./pages/Home.jsx" 
+import Contact from "./components/Contact";
 
-function App() {
-  return (
-    <>
-      <div>
-        You import the pages here.
-        <Homepage />
-      </div>
-    </>
-  )
+
+function App(){
+    return(
+        <>
+        <Contact/>
+        </>
+    )
 }
 
-export default App
+export default App;
