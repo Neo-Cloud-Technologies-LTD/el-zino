c2e42679de12b3e054bf11e4587a4e14c35f123e
rishsz <anthonyobinnaorji@gmail.com>
2026-08-12 16:07:51 +0100

    Update contact page

--- frontend/elzino-app/src/components/contact.jsx
+++ frontend/elzino-app/src/components/contact.jsx
@@ -1,11 +1,72 @@
 import "./contact.css";
 
-function Contact(){
-    return(
+function Contact() {
+    return (
         <>
-        This is the Contact component. Your html code for the Contact component goes here.
+            <fieldset>
+                <h2>Inquiries</h2>
+                <p>
+                    For any inquiries, questions, or quotes, please call:
+                    123-456-7890 or fill out the following form
+                </p>
+
+                <h2>Contact Us</h2>
+
+                <form action="">
+                    <label htmlFor="fname">
+                        First Name<span>*</span>
+                    </label>
+                    <input
+                        type="text"
+                        id="fname"
+                        name="fname"
+                        required
+                    />
+
+                    <label htmlFor="lname">
+                        Last Name<span>*</span>
+                    </label>
+                    <input
+                        type="text"
+                        id="lname"
+                        name="lname"
+                        required
+                    />
+
+                    <label htmlFor="email">
+                        Email<span>*</span>
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        required
+                    />
+
+                    <label htmlFor="text">
+                        Subject<span>*</span>
+                    </label>
+                    <input
+                        type="text"
+                        id="text"
+                        name="text"
+                        required
+                    />
+
+                    <label htmlFor="message">
+                        Message<span>*</span>
+                    </label>
+                    <textarea
+                        name="message"
+                        id="message"
+                        required
+                    ></textarea>
+
+                    <button type="submit">Submit</button>
+                </form>
+            </fieldset>
         </>
-    )
+    );
 }
 
 export default Contact;
