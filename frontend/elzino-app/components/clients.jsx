a1d83201bbfc9d8a85b1a35862f47381fe87a5f5
marisssm <mariyaaaminu12@gmail.com>
2026-08-16 19:30:48 +0100

    moved the html and css code to components

---
+++ frontend/elzino-app/components/clients.jsx
@@ -0,0 +1,20 @@
+import "./clients.css";
+
+function Client(){
+    return(
+        <>
+         <div id="root">
+    <h2 class="Client1">Our Clients</h2>
+        <div class="Client-row">
+         <img src="src/assets/tipox.png" alt="TIPOX"/>
+         <img src="src/assets/excipia.png" alt="EXCIPIA"/>
+         <img src="src/assets/vide.png" alt="Vide"/>
+         <img src="src/assets/dehotel.png" alt="DEHOTEL"/>
+         <img src="src/assets/umber.png" alt="UMBER & CO"/>
+        </div>
+         </div>
+        </>
+    )
+}
+
+export default Client;
